6787695fbedeb887137275dbf44a2cde926fcec6
Valerie Houseman <valeriehouseman@ozarkVal.local>
2019-07-12 19:50:16 -0500

    Commit the rest of it

---
+++ src/main/java/org/codergirl/java/bookshelf/BookshelfApplication.java
@@ -0,0 +1,13 @@
+package org.codergirl.java.bookshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookshelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/codergirl/java/bookshelf/BookshelfApplicationTests.java
@@ -0,0 +1,16 @@
+package org.codergirl.java.bookshelf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookshelfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
