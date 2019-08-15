275a292218005e9ed87d49acc21893dab7ff5878
Valerie Houseman <valeriehouseman@ozarkVal.local>
2019-08-14 19:39:49 -0500

    Add ability to read to H2 locally

---
+++ src/main/db/bookshelf_init.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA bookshelf authorization sa;
+
+CREATE TABLE bookshelf.bookmark (
+ID INT PRIMARY KEY,
+TITLE VARCHAR(255) DEFAULT '',
+ADDRESS VARCHAR(255) DEFAULT '');
+
+INSERT INTO bookshelf.bookmark VALUES (1, '[Khan Academy] Intro to SQL: Querying and managing data', 'https://www.khanacademy.org/computing/computer-programming/sql');
+
+INSERT INTO bookshelf.bookmark VALUES (2, 'https://www.youtube.com/watch?v=40KM8IdneLg&list=PLEAQNNR8IlB588DQvb2wbKFQh2DviPApl', 'https://www.youtube.com/watch?v=40KM8IdneLg&list=PLEAQNNR8IlB588DQvb2wbKFQh2DviPApl');
+
+select * from bookshelf.bookmark;
